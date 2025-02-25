99ec8d084cafac0cfa73fc6843b14edb39be9520
i.kazantcev <i.kazantcev@bftcom.com>
2025-02-25 19:31:47 +0300

    Добавление теста аватарки

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/ProfileTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/ProfileTest.java
@@ -3,6 +3,7 @@ package guru.qa.niffler.test.web;
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.annotation.Category;
+import guru.qa.niffler.jupiter.annotation.ScreenShotTest;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
 import guru.qa.niffler.model.CategoryJson;
@@ -10,8 +11,16 @@ import guru.qa.niffler.model.UserJson;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.page.ProfilePage;
 import guru.qa.niffler.utils.RandomDataUtils;
+import guru.qa.niffler.utils.ScreenDiffResult;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
 @WebTest
 public class ProfileTest {
@@ -24,7 +33,7 @@ public class ProfileTest {
   )
   @DisplayName("Архивная категория должна присутствовать и отображаться в списке категорий")
   @Test
-  void archivedCategoryShouldPresentInCategoriesList(CategoryJson[] category) {
+  void archivedCategoryShouldPresentInCategoriesList(@NotNull CategoryJson[] category) {
     Selenide.open(CFG.frontUrl(), LoginPage.class)
             .login(category[0].username(), "123")
             .navigateMenuComponent
@@ -40,7 +49,7 @@ public class ProfileTest {
   )
   @DisplayName("Активная категория должна присутствовать и отображаться в списке категорий")
   @Test
-  void activeCategoryShouldPresentInCategoriesList(CategoryJson category) {
+  void activeCategoryShouldPresentInCategoriesList(@NotNull CategoryJson category) {
     Selenide.open(CFG.frontUrl(), LoginPage.class)
             .login("ivan", "123")
             .navigateMenuComponent
@@ -50,7 +59,7 @@ public class ProfileTest {
   }
   @User
   @Test
-  void updateAllFieldsProfile(UserJson user) {
+  void updateAllFieldsProfile(@NotNull UserJson user) {
     new LoginPage()
             .open()
             .login(user.username(), user.testData().password())
@@ -61,5 +70,24 @@ public class ProfileTest {
             .setNewCategory(RandomDataUtils.randomCategoryName())
             .saveChanges()
             .checkAlertMessage("Profile successfully updated");
+  }
+
+  @User
+  @DisplayName("Проверка загрузки аватарки")
+  @ScreenShotTest(value = "image/expected-avatar.png")
+  void checkCorrectUploadAvatar(@NotNull UserJson user, BufferedImage expected) throws IOException {
+    new LoginPage()
+            .open()
+            .login(user.username(), user.testData().password())
+            .getHeader()
+            .toProfilePage()
+            .uploadImage("resources/img/bait.png")
+            .saveChanges();
+
+    BufferedImage actual = ImageIO.read(Objects.requireNonNull(new ProfilePage().getAvatar().screenshot()));
+    Assertions.assertFalse(new ScreenDiffResult(
+            actual,
+            expected
+    ));
   }
 }
